a3873a850dc8d7ef147420016cebb51e0c4afce7
Carl Turpie <carl.turpie@gmail.com>
2016-10-08 16:20:35 +0100

    updates based on progress on udacity

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,35 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+count = 0
+for name in enron_data:
+    if enron_data[name]["poi"] == 1:
+        count = count + 1
 
+print count
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]["total_payments"]
+
+count = 0
+for name in enron_data:
+#    if enron_data[name]["poi"] == 1:
+        if enron_data[name]["total_payments"] == "NaN":
+            count = count + 1
+
+print count
+print float(count) / len(enron_data)
